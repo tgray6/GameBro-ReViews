2422465113ec717164558599af12f23c0a100113
Xer0 <tylergrayx@gmail.com>
2018-01-25 16:02:10 -0500

    Test commit for MLAB database

--- config.js
+++ config.js
@@ -2,4 +2,6 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/GameBro';
 
+const DATABASE_URL = "mongodb://tgray6:sagesage1@ds113648.mlab.com:13648/gamebro";
+
 exports.PORT = process.env.PORT || 8080;
