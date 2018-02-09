558c1795a2d3800e3efa5446e4486b62048d4bef
Xer0 <tylergrayx@gmail.com>
2018-02-09 12:42:52 -0500

    heroku working, testing TEST_DATABASE_URL

--- config.js
+++ config.js
@@ -1,5 +1,12 @@
 'use strict';
 
-exports.DATABASE_URL = 'mongodb://tgray6:sagesage1@ds113648.mlab.com:13648/gamebro';
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/GameBroTest';
+// exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/GameBro';
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/GameBroTest';
+// exports.PORT = process.env.PORT || 8080;
+
+
+
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://tgray6:sagesage1@ds113648.mlab.com:13648/gamebro';
+
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://tgray6:sagesage1@ds113648.mlab.com:121543/gamebrotest';;
 exports.PORT = process.env.PORT || 8080;
