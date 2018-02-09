9725ff778098e1a0a15423d61e776915452d56ea
Xer0 <tylergrayx@gmail.com>
2018-02-09 12:45:09 -0500

    fixed port on TEST_URL

--- config.js
+++ config.js
@@ -8,5 +8,5 @@
 
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://tgray6:sagesage1@ds113648.mlab.com:13648/gamebro';
 
-exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://tgray6:sagesage1@ds113648.mlab.com:121543/gamebrotest';;
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://tgray6:sagesage1@ds121543.mlab.com:21543/gamebrotest';;
 exports.PORT = process.env.PORT || 8080;
